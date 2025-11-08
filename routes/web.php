482d32b92b54d20ba3fef99d8bac4a842d9c1ce8
Derrick Betts <derrick@blueaxis.com>
2025-11-08 10:42:15 -0700

    Bug fix in routes.

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\PublicLeadCaptureController;
 use App\Http\Controllers\MemberPortalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('app');
-});
+Route::get('/', fn () => view('app'));
 
 Route::get('/lead/capture', [PublicLeadCaptureController::class, 'form']);
 Route::post('/public/leads', [PublicLeadCaptureController::class, 'store']);
 Route::get('/member/portal', [MemberPortalController::class, 'show']);
 Route::post('/member/portal/goals', [MemberPortalController::class, 'storeGoal']);
+
+Route::fallback(fn () => view('app'));
